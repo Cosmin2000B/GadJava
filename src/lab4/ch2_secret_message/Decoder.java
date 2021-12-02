eb1050f1355ba0ab20cdaeb9a681d00899484897
Cosmin2000B <cosmin.ionescu@s.utm.ro>
2021-12-02 03:16:40 +0200

    ch2

---
+++ src/lab4/ch2_secret_message/Decoder.java
@@ -0,0 +1,51 @@
+package lab4.ch2_secret_message;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Decoder
+{
+    private String secretMessage;
+
+    public Decoder(String inputFile)
+    {
+        Scanner from = null;
+
+        try
+        {
+            from = new Scanner(new BufferedReader(new FileReader(inputFile)));
+
+            secretMessage = "";
+
+            while(from.hasNext())
+            {
+                String word = from.next();
+                char[] wordC = word.toCharArray();
+
+                for(char c: wordC)
+                {
+                    if (c == 'X')
+                        secretMessage += ' ';
+                    else if ('A' <= c && c <= 'Z')
+                        secretMessage += c;
+                }
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println(e.fillInStackTrace());
+        }
+        finally
+        {
+            if(from != null)
+                from.close();
+        }
+    }
+
+    public String getSecretMessage()
+    {
+        return this.secretMessage;
+    }
+}
---
+++ src/lab4/ch2_secret_message/Main.java
@@ -0,0 +1,17 @@
+package lab4.ch2_secret_message;
+
+import java.util.List;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Decoder decoder01 = new Decoder("./src/lab4/ch2_secret_message/coded_message.TXT");
+        System.out.println(decoder01.getSecretMessage());
+
+        System.out.println();
+
+        Decoder decoder02 = new Decoder("./src/lab4/ch2_secret_message/example_text.TXT");
+        System.out.println(decoder02.getSecretMessage());
+    }
+}
