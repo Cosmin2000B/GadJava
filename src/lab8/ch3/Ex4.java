ac9b59aee8c09a96280653ff57fa8dc306f142e2
Cosmin2000B <cosmin.ionescu@s.utm.ro>
2021-12-08 21:52:15 +0200

    ex4

--- src/lab8/ch3/Ex4.java
+++ src/lab8/ch3/Ex4.java
@@ -1,11 +1,27 @@
 package lab8.ch3;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Ex4
 {
-    public static List<String> mapToString(List<String> words)
+    public static String mapToString(Map<Integer, String> words)
     {
+        return words.entrySet()
+                .stream()
+                .map(s -> s.getKey() + s.getValue())
+                .collect(Collectors.joining(", "));
+    }
 
+    public static void main(String[] args)
+    {
+        Map<Integer, String> myMap = new HashMap<>();
+        myMap.put(1, "One");
+        myMap.put(2, "Two");
+        myMap.put(3, "Three");
+
+        System.out.println(mapToString(myMap));
     }
 }
