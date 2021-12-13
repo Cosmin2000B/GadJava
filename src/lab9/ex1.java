2a02741d2a99011579276aebe90abb7ee65c1ddb
Cosmin2000B <cosmin.ionescu@s.utm.ro>
2021-12-13 20:27:00 +0200

    lab9

---
+++ src/lab9/ex1.java
@@ -0,0 +1,22 @@
+package lab9;
+
+import java.util.stream.IntStream;
+
+public class ex1
+{
+    public static void main(String[] args)
+    {
+        int factor = 2;
+        IntStream s = IntStream
+                .range(0, 1000)
+                .map(e ->
+                {
+                    System.out.println("e=" + e);
+                    return e *  factor;
+                })
+                .filter(e -> e > 5);
+
+        int first = s.findFirst().getAsInt();
+        System.out.println(first);
+    }
+}
